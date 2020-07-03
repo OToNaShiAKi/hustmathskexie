0134af8b62ac3027d93e71ca350dcdea56389e54
bomb <bombeuler@163.com>
2020-07-03 16:44:58 +0800

    update contact

--- miniprogram/pages/contact/contact.js
+++ miniprogram/pages/contact/contact.js
@@ -5,20 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    departContact:[]
+    departContact: []
   },
+  getContacts(){
+    wx.showLoading({
+      title: '加载中'
+    })
+    wx.cloud.callFunction({
+      name: "getContact",
+    }).then(res => {
+      const departContact = res.result;
+      this.setData({
+        departContact
+      });
+      wx.showToast({
+        title: '加载完成'
+      })
 
+    }).catch(err => {
+      wx.showToast({
+        title: '加载失败，请确认网络后重试',
+        icon: 'none'
+      })
+      console.log(err);
+
+    }).finally(wx.hideLoading)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.cloud.callFunction({
-      name:"getContact",
-    }).then(res=>{
-      
-      console.log(res);
-      
-    })
+   this.getContacts();
   },
 
   /**
@@ -53,14 +70,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+   
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    // this.getContacts();
   },
 
   /**

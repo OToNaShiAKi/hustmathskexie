ae474dae436320105198644e9fd1a2bf6b373648
bomb <bombeuler@163.com>
2020-07-03 23:56:22 +0800

    upadate to-do wxss

--- miniprogram/pages/infoDetail/infoDetail.js
+++ miniprogram/pages/infoDetail/infoDetail.js
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var info;
+    let info;
     console.log(options);
     wx.cloud.callFunction({
       name: 'selectStudentById',
--- miniprogram/pages/selectStudent/selectStudent.js
+++ miniprogram/pages/selectStudent/selectStudent.js
@@ -1,6 +1,6 @@
 // miniprogram/pages/selectStudent/selectStudent.js
 import { DepartFormat } from './../../utils/Format'
-
+import { DepartColor } from './../../utils/FormatColor';
 
 Page({
 
@@ -8,23 +8,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    depart: { key: '', name: ''},
-    interview:{ num:0,list:[] },
-    written:{ num:0,list:[] },
-    operation:{ num:0,list:[] },
-    showIndex:0
+    buttonColor:"",
+    depart: { key: '', name: '' },
+    interview: { num: 0, list: [] },
+    written: { num: 0, list: [] },
+    operation: { num: 0, list: [] },
+    showIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      const depart = this.data.depart
-      const key = options.department;
-      depart.key = key;
-      depart.name = DepartFormat(key);
-      this.selectStudent(depart.name);
-      this.setData({ depart });
+    const depart = this.data.depart;
+    const key = options.department;
+    const buttonColor = DepartColor(key);
+    depart.key = key;
+    depart.name = DepartFormat(key);
+    this.selectStudent(depart.name);
+    this.setData({ depart , buttonColor });
   },
 
   panel: function (e) {
@@ -40,70 +42,70 @@ Page({
     }
   },
 
-  selectStudent:function(name){
-    let result,data,interview,written,operation;
-    interview={
-      num:0,
-      list:[]
+  selectStudent: function (name) {
+    let result, data, interview, written, operation;
+    interview = {
+      num: 0,
+      list: []
     };
-    written={
-      num:0,
-      list:[]
+    written = {
+      num: 0,
+      list: []
     };
-    operation={
-      num:0,
-      list:[]
+    operation = {
+      num: 0,
+      list: []
     };
     wx.cloud.callFunction({
       name: 'selectStudent',
-      data:{
-        name:name
+      data: {
+        name: name
       }
-    }).then(res=>{
-        result=res.result;
-        data=result.result.res.data;
-        for(let i=0;i<data.length;i++){
-          switch(data[i].status){
-            case 0:
-              interview.num++;
-              interview.list.push(data[i]);
+    }).then(res => {
+      result = res.result;
+      data = result.result.res.data;
+      for (let i = 0; i < data.length; i++) {
+        switch (data[i].status) {
+          case 0:
+            interview.num++;
+            interview.list.push(data[i]);
             break;
-            case 2:
-              written.num++;
-              written.list.push(data[i]);
+          case 2:
+            written.num++;
+            written.list.push(data[i]);
             break;
-            case 4:
-              operation.num++;
-              operation.list.push(data[i]);
+          case 4:
+            operation.num++;
+            operation.list.push(data[i]);
             break;
-          }
         }
-      console.log(data,res)
-      this.setData({ interview,written,operation });
-      })
+      }
+      console.log(data, res)
+      this.setData({ interview, written, operation });
+    })
   },
 
-  linkto:function(e){
+  linkto: function (e) {
     console.log(e);
     var button;
-    if(e.target.dataset.name){
+    if (e.target.dataset.name) {
       wx.navigateTo({
         url: '/pages/infoDetail/infoDetail?id=' + e.currentTarget.dataset.id
       })
-    }else if(button=e.target.dataset.button){
+    } else if (button = e.target.dataset.button) {
       console.log(button);
       wx.cloud.callFunction({
         name: "selectPassOrNot",
-        data:{
-          choice:button,
-          id:e.currentTarget.dataset.id
+        data: {
+          choice: button,
+          id: e.currentTarget.dataset.id
         }
-      }).then(res=>{
-        this.selectStudent();
+      }).then(res => {
+        this.selectStudent(this.data.depart.name);
         console.log(res);
       })
     }
-    
+
   },
 
   /**

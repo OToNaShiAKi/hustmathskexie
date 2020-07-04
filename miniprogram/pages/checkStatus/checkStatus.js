c3f52a61637f861086067403350a30251352966b
cyc <770161366@qq.com>
2020-07-04 21:17:07 +0800

    显示审核情况制作完毕（v1.0）

--- miniprogram/pages/checkStatus/checkStatus.js
+++ miniprogram/pages/checkStatus/checkStatus.js
@@ -33,25 +33,82 @@ Page({
     name: 'cyc',
     phone: '1234',
     department: {},
-    steps: [
-      {
-        text: '报名',
-        desc: '核验报名信息数据',
-      },
-      {
-        text: '步骤二',
-        desc: '描述信息',
-      },
-      {
-        text: '步骤三',
-        desc: '描述信息',
-      },
-      {
-        text: '步骤四',
-        desc: '描述信息',
-      },
-    ],
-    active: 2
+    
+    steps: {
+        'editor':[
+          {
+          text: '报名成功',
+          desc: '待审核',
+        },
+        {
+          text: '面试',
+          desc: '面试已过，等待笔试通知',
+        },
+        {
+          text: '笔试',
+          desc: '笔试已通过，onecho等待实操',
+        },
+        {
+          text: '实操',
+          desc: '全部考核通过，入部成功',
+        },
+      ],
+        "office":[
+          {
+          text: '报名成功',
+          desc: '待审核',
+        },
+        {
+          text: '面试',
+          desc: '面试已过，等待笔试通知',
+        },
+        {
+          text: '笔试',
+          desc: '笔试已通过，onecho等待实操',
+        },
+        {
+          text: '实操',
+          desc: '全部考核通过，入部成功',
+        },
+      ],
+        "onecho":[
+          {
+          text: '报名成功',
+          desc: '待审核',
+        },
+        {
+          text: '面试',
+          desc: '面试已过，等待笔试通知',
+        },
+        {
+          text: '笔试',
+          desc: '笔试已通过，onecho等待实操',
+        },
+        {
+          text: '实操',
+          desc: '全部考核通过，入部成功',
+        },
+      ],
+        "workshop":[
+          {
+          text: '报名成功',
+          desc: '待审核',
+        },
+        {
+          text: '面试',
+          desc: '面试已过，等待笔试通知',
+        },
+        {
+          text: '笔试',
+          desc: '笔试已通过，onecho等待实操',
+        },
+        {
+          text: '实操',
+          desc: '全部考核通过，入部成功',
+        },
+      ],
+    },
+    active:0
   },
 
   onLoad(options) {
@@ -82,9 +139,33 @@ Page({
     // }
 
     Query({ name, phone }, data => {
-      console.log(data);
+      console.log(this.data);
       const { department, status } = data;
-      this.setData({ department,status })
+      const { steps }=this.data;
+      for(let item in department){
+        if(status[department[item].name]%2==1){
+          status[department[item].name]=Math.ceil(status[department[item].name]/2);
+          let s=status[department[item].name];
+          steps[item][s].desc=steps[item][s].text+"未通过";
+          steps[item][s].activeIcon='cross';
+          steps[item][s].activeColor='#ff0000';
+          for(let i=s+1;i<4;i++){
+            steps[item].pop();
+          }
+        }else{
+          status[department[item].name]/=2;
+          let s=status[department[item].name];
+          steps[item][s].activeIcon='success';
+          steps[item][s].activeColor='#38f';
+          steps[item][s+1].desc='';
+          for(let i=s+2;i<4;i++){
+            steps[item].pop();
+          }
+        }
+        console.log(item,steps[item]);
+      }
+      console.log(status,steps);
+      this.setData({ department,status,steps })
     })
   },
 
--- miniprogram/pages/datetest/datetest.js
+++ miniprogram/pages/datetest/datetest.js
@@ -1,55 +1,32 @@
 // miniprogram/pages/datetest/datetest.js
 Page({
   data: {
-    minHour: 10,
-    maxHour: 20,
-    minDate: new Date().getTime(),
-    maxDate: new Date().getTime()+5184000000,//最多两个月
-    currentDate: new Date().getTime(),
-    formatter(type, value) {
-      if (type === 'year') {
-        return `${value}年`;
-      } else if (type === 'month') {
-        return `${value}月`;
-      }else if (type === 'day'){
-        return `${value}日`;
-      }else if(type === 'hour'){
-        return `${value}时`;
-      }else if(type === 'minute'){
-        return `${value}分`;
-      }
-      return value;
-    },
-    date: '',
-    show: false,
-  },
-
-  onInput(event) {
-    this.setData({
-      currentDate: event.detail,
-    });
-  },
-  onDisplay() {
-    this.setData({ show: true });
-  },
-  onClose() {
-    this.setData({ show: false });
-  },
-  formatDate(date) {
-    date = new Date(date);
-    var minute;
-    if(date.getMinutes()<10){
-      minute ='0' + date.getMinutes();
-    }else{
-      minute=date.getMinutes();
-    }
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getHours()}:${minute}`;
-  },
-  onConfirm(event) {
-    this.setData({
-      show: false,
-      date: this.formatDate(event.detail),
-    });
-    console.log(event.detail);
+    steps: [
+      {
+        text: '步骤一',
+        desc: '描述信息',
+        inactiveIcon: 'location-o',
+        activeIcon: 'success',
+      },
+      {
+        text: '步骤二',
+        desc: '描述信息',
+        inactiveIcon: 'like-o',
+        activeIcon: 'plus',
+      },
+      {
+        text: '步骤三',
+        desc: '描述信息',
+        inactiveIcon: 'star-o',
+        activeIcon: 'cross',
+      },
+      {
+        text: '步骤四',
+        desc: '描述信息',
+        inactiveIcon: 'phone-o',
+        activeIcon: 'fail',
+      },
+    ],
+    active:2
   },
 });

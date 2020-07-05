d0a8bbc058b15ca028a9ae9cef423631160eafca
bomb <bombeuler@163.com>
2020-07-05 15:05:45 +0800

    联系部门板块基本完成

--- miniprogram/components/inforSlip/inforSlip.js
+++ miniprogram/components/inforSlip/inforSlip.js
@@ -4,35 +4,85 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    title:{
-      type:String,
-      value:"标题"
+    title: {
+      type: String,
+      value: "标题"
     },
-    content:{
-      type:String|Number,
-      value:"内容"
+    content: {
+      type: String | Number,
+      value: "内容"
     },
-    oneText:{
-      type:Boolean,
-      value:false
+    oneText: {
+      type: Boolean,
+      value: false
     },
-    oneContent:{
-      type:String|Number,
-      value:"我不可复制"
-    }
+    oneContent: {
+      type: String | Number,
+      value: "我不可复制"
+    },
+    eventBreak: {
+      type: Boolean,
+      value: false
+    },
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    textColor: "unset",
+    flag: true
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    changeColor(event) {
+      let timeSet = undefined;
+      clearInterval(timeSet);
+      let time = 0;
+      
+      this.setData({
+        textColor: "#dfe6e9",
+        flag: false
+      });
+      timeSet = setInterval(() => {
+        time = time + 10;
+        if (this.data.flag) {
+          clearInterval(timeSet);
+        } else {
+          if (time >= 500) {
+            wx.setClipboardData({
+              data: event.currentTarget.dataset.ctn.toString()
+            }).then(res => {
+              console.log(res);
 
+              wx.showToast({
+                title: "已复制到剪切板",
+                duration: 800
+              })
+            }).catch(err => {
+              console.log(err);
+              wx.showToast({
+                title: '复制失败',
+              });
+            });
+            this.setData({
+              textColor: "unset"
+            });
+            clearInterval(timeSet);
+          }
+        }
+      }, 10);
+
+    },
+    reColor(event) {
+      this.setData({
+        textColor: "unset",
+        flag: true
+      });
+      
+    }
   }
 })
--- miniprogram/pages/contact/contact.js
+++ miniprogram/pages/contact/contact.js
@@ -10,28 +10,13 @@ Page({
   data: {
     departContact: [],
     tabActive: 0,
-    showPop:false
-  },
-  async compareTime(year, date) {
-    const dateList = date.split("/");
-    let past = `${year}/${dateList[0].padStart(2,"0")}/${dateList[1].padStart(2, "0")} ${dateList[2].padStart(4, "0")}:00`;
-    let datepast = new Date(past);
-    let now ;
-    await wx.cloud.callFunction({
-      name:"getTimes"
-    }).then(res =>{
-      now = new Date(res.result);
-    });
-    return new Promise((resolve,reject)=>{
-     const judge = (datepast.getTime() >= now.getTime()) ? true : false;
-     resolve(judge);
-    })
+    showPop: false
   },
   getContacts() {
     wx.showLoading({
       title: '加载中'
     });
-    
+
     wx.cloud.callFunction({
       name: "getContact",
     }).then(res => {
@@ -42,20 +27,20 @@ Page({
         ele.canDownload = (ele.imgUrl === "none") ? false : true;
         ele.imgUrl = (ele.imgUrl === "none") ? noImgUrl : ele.imgUrl;
       });
-      
+
       this.setData({
         departContact
       });
       wx.showToast({
         title: '加载完成',
-        duration:800
+        duration: 800
       })
 
     }).catch(err => {
       wx.showToast({
         title: '加载失败，请确认网络后重试',
         icon: 'none',
-        duration:800
+        duration: 800
       })
       console.log(err);
     }).finally(wx.hideLoading);
@@ -64,33 +49,55 @@ Page({
    * 事件函数
    */
   onClose() {
-    this.setData({ showPop: false });
+    this.setData({
+      showPop: false
+    });
   },
-   outBox(event){
-     const canDownload=event.currentTarget.dataset.flag;
-     if(!canDownload){return;}
-     this.setData({showPop:true});
-   },
+  outBox(event) {
+    const canDownload = event.currentTarget.dataset.flag;
+    if (!canDownload) {
+      return;
+    }
+    this.setData({
+      showPop: true
+    });
+  },
 
-scan(event){
-//TODO
-},
-
-download(event){
-//TODO
-},
+  //扫描二维码
+  
+  //下载二维码
+  download(event) {
+    const img = event.currentTarget.dataset.img;
+    this.setData({showPop:false});
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.cloud.downloadFile({
+      fileID: img
+    }).then(res => {
+      return wx.saveImageToPhotosAlbum({
+        filePath: res.tempFilePath,
+      })
+    }).then(res => {
+      
+      wx.showToast({
+        title: '保存成功',
+        icon: 'success',
+        duration: 1000
+      }).catch(error => {
+        wx.showToast({
+          title: '保存失败，请检查网络状态',
+          duration: 1000
+        }).finally(wx.hideLoading);
+      })
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.getContacts();
-this.compareTime(2020,"7/8/18:54").then(res=>{
-      console.log(res);
-      
-    });
-    // console.log(n);
-    
   },
 
   /**
